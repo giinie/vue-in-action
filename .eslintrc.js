70499e252d4fe49cdb8df5ccc50e0b5c66b7f8bf
giinie <giinie72@gmail.com>
2024-01-19 16:26:35 +0900

    chapter-07 준비

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'airbnb',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
--- Training/TestComponent.vue
+++ Training/TestComponent.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="hello">
+    <h1>Test Component</h1>
     {{ msg }}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'single-file-component',
+  name: 'TestComponent',
   data() {
     return {
-      msg: 'Vue.js 앱에 오신걸 환영합니다',
+      msg: 'Vue.js 앱에 오신 걸 환영합니다.',
     };
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped></style>
