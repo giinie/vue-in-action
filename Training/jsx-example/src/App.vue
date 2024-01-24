dca05762db544398c8a98494e8bb329bec1fba83
giinie <giinie72@gmail.com>
2024-01-24 14:57:40 +0900

    chapter-09 완료

---
+++ Training/jsx-example/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <HelloWorld header="1" name="에릭" />
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line import/extensions,import/no-unresolved
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style></style>
---
+++ Training/jsx-example/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "HelloWorld",
+  render(h) {
+    return (
+      <div>
+        <div
+          class="text-center"
+          on-click={this.pressed}
+          domPropsInnerHTML={this.msg}
+        />
+      </div>
+    );
+  },
+  data() {
+    return {
+      msg: `<h${this.header}>안녕하세요 ${this.name}님</h${this.header}>`
+    };
+  },
+  methods: {
+    pressed() {
+      // eslint-disable-next-line no-alert
+      alert("클릭했어요");
+    }
+  },
+  props: ["header", "name"]
+};
+</script>
